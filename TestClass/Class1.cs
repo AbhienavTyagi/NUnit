cd89ea2b20840786ea7cbf41a842a694dd04a072
Abhinav Tyagi <Atyagi4516@conestogac.on.ca>
2024-06-11 03:46:13 -0400

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,25 @@
+﻿
+
+using NUnit.Framework;
+using NUnit;
+namespace TestClass
+{
+    [TestFixture]
+    public class TriangleTest1
+    {
+        // Abhinav Tyagi
+        [Test]
+        public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+        {
+            // Arrange
+            int firstAngle = 60;
+            int secondAngle = 90;
+            int thirdAngle = 30;
+            string expected = "The triangle is valid.";
+            // Act
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
